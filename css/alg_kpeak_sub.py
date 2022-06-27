7cb9c21364e4f8940583de5d084c71287af7a340
cscnudi <106224155+cscnudi@users.noreply.github.com>
2022-06-27 18:54:53 +0900

    Update alg_kpeak_sub.py

--- css/alg_kpeak_sub.py
+++ css/alg_kpeak_sub.py
@@ -1,10 +1,6 @@
 __author__ = 'priya'
 
 import networkx as nx
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import rcParams
-rcParams['figure.figsize'] = 10,7
-
 
 # Function that computes peak numbers (i.e. performs k-peak decomposition)
 def get_kpeak_decomposition(G):
@@ -174,110 +170,9 @@ def get_kpeak_mountainassignment_component(G, orig_core_nums):
     return permountain_ID_corenumber_peaknumber, peak_numbers
 
 
-
-
-# Function draws a mountain plot, given the peak numbers, core numbers and mountain assignment
-def plot_mountains_given_mountainassignment_givencomponents(permountain_ID_core_peak_numbers, orig_core_nums, peak_numbers, graphname):
-
-    ### Part 1 ####
-    # Sorting the nodes in each mountain
-    # The final ordering is such that nodes are ordered in descending order of core number
-    # The nodes with same core umber in a mountain are ordered (in descending order) of their peak number
-    # Arranging the values in arrays, of x and y axis to be plotted based on above ordering
-    x_vals = []; y_vals = []; y_vals2 = []; nodecount=0
-    mountain_breaks_x_vals = []
-    mountain_breaks_y_vals = []
-    for id in permountain_ID_core_peak_numbers:
-        mountaindict = permountain_ID_core_peak_numbers[id]
-        unsorted_tuples = mountaindict.values()
-        sortedbypeaknumber_tuples = sorted(unsorted_tuples, key=lambda xyv: xyv[2], reverse=True)
-        sortedbyCOREnumber_tuples = sorted(sortedbypeaknumber_tuples, key=lambda xyv: xyv[1], reverse=True)
-        # node_ordering_permountain[id] = [x for x, y, z in sortedbyCOREnumber_tuples]
-        nodelist_this_mountain = [x for x, y, z in sortedbyCOREnumber_tuples]
-        for i in range(len(nodelist_this_mountain)):
-            x_vals.append(nodecount); nodecount+=1
-            y_vals.append(orig_core_nums[nodelist_this_mountain[i]])
-            y_vals2.append(peak_numbers[nodelist_this_mountain[i]])
-        mountain_breaks_x_vals.append(nodecount)
-        mountain_breaks_y_vals.append(y_vals[-1])
-    ### Part 2 ####
-    ## The plotting
-    ax = plt.gca()
-    plt.fill_between(x_vals, y_vals, 0, color = 'lightblue') # Area under the core number values (blue line)
-    plt.plot(x_vals, y_vals, label = 'Core Number', color = 'blue')
-    plt.scatter(x_vals, y_vals2, color = 'r', label = 'Peak Number')
-    for i in range(len(mountain_breaks_x_vals)):
-        if i == len(mountain_breaks_x_vals) - 1:
-            plt.plot([mountain_breaks_x_vals[i]-1, mountain_breaks_x_vals[i]-1],[0, mountain_breaks_y_vals[i]], color = 'black', label = 'Boundary Between Mountains')
-        else:
-            plt.plot([mountain_breaks_x_vals[i]-1, mountain_breaks_x_vals[i]-1],[0, mountain_breaks_y_vals[i]], color = 'black')
-    plt.ylabel('Core Number or Peak Number', fontsize=20); plt.xlabel('Individual nodes', fontsize=20)
-    plt.legend(fontsize=18,bbox_to_anchor=(1.01, 1), prop={'size':18})
-    plt.xlim(0, len(orig_core_nums.keys()))
-    plt.ylim(0, max([orig_core_nums[x] for x in orig_core_nums]))
-    ax.tick_params(axis='x', labelsize=18); ax.tick_params(axis='y', labelsize=18)
-    # plt.show()
-    plt.savefig(graphname+'_mountainplot_withcomponents_and_mountainmarkers.pdf', bbox_inches='tight')
-
-    plt.close()
-
-
-def plot_mountains_given_mountainassignment(permountain_ID_core_peak_numbers, orig_core_nums, peak_numbers, graphname):
-
-    ### Part 1 ####
-    # Sorting the nodes in each mountain
-    # The final ordering is such that nodes are ordered in descending order of core number
-    # The nodes with same core umber in a mountain are ordered (in descending order) of their peak number
-    # Arranging the values in arrays, of x and y axis to be plotted based on above ordering
-    x_vals = []; y_vals = []; y_vals2 = []; nodecount=0
-    for id in permountain_ID_core_peak_numbers:
-        mountaindict = permountain_ID_core_peak_numbers[id]
-        unsorted_tuples = mountaindict.values()
-        sortedbypeaknumber_tuples = sorted(unsorted_tuples, key=lambda xyv: xyv[2], reverse=True)
-        sortedbyCOREnumber_tuples = sorted(sortedbypeaknumber_tuples, key=lambda xyv: xyv[1], reverse=True)
-        # node_ordering_permountain[id] = [x for x, y, z in sortedbyCOREnumber_tuples]
-        nodelist_this_mountain = [x for x, y, z in sortedbyCOREnumber_tuples]
-        for i in range(len(nodelist_this_mountain)):
-            x_vals.append(nodecount); nodecount+=1
-            y_vals.append(orig_core_nums[nodelist_this_mountain[i]])
-            y_vals2.append(peak_numbers[nodelist_this_mountain[i]])
-    ### Part 2 ####
-    ## The plotting
-    ax = plt.gca()
-    plt.fill_between(x_vals, y_vals, 0, color = 'lightblue') # Area under the core number values (blue line)
-    plt.plot(x_vals, y_vals, label = 'Core Number', color = 'blue')
-    plt.scatter(x_vals, y_vals2, color = 'r', label = 'Peak Number')
-    plt.ylabel('Core Number or Peak Number', fontsize=20); plt.xlabel('Individual nodes', fontsize=20)
-    plt.legend(fontsize=18,bbox_to_anchor=(1.01, 1), prop={'size':18})
-    plt.xlim(0, len(orig_core_nums.keys()))
-    plt.ylim(0, max([orig_core_nums[x] for x in orig_core_nums]))
-    ax.tick_params(axis='x', labelsize=18); ax.tick_params(axis='y', labelsize=18)
-    # plt.show()
-    plt.savefig(graphname+'_mountainplot.pdf', bbox_inches='tight')
-
-    plt.close()
-
-# Function that computes peak numbers and then makes mountain plot
-def draw_mountainplot(G, graphname):
-    G.remove_edges_from(G.selfloop_edges())
-    G = removeSingletons(G)
-    orig_core_nums = nx.core_number(G)
-    permountain_ID_corenumber_peaknumber, peak_numbers = get_kpeak_mountainassignment(G, orig_core_nums)
-    plot_mountains_given_mountainassignment(permountain_ID_corenumber_peaknumber, orig_core_nums, peak_numbers, graphname)
-
-def draw_mountainplot_components(G, graphname):
-    G.remove_edges_from(G.selfloop_edges())
-    G = removeSingletons(G)
-    orig_core_nums = nx.core_number(G)
-    permountain_ID_corenumber_peaknumber, peak_numbers = get_kpeak_mountainassignment_component(G, orig_core_nums)
-    plot_mountains_given_mountainassignment_givencomponents(permountain_ID_corenumber_peaknumber, orig_core_nums, peak_numbers, graphname)
-
-
 def removeSingletons(G):
     degrees=G.degree()
     for node in degrees.keys():
         if degrees[node]==0:
             G.remove_node(node)
     return G
-
-
