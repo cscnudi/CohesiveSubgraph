645e320e7680160facfad9e365ee99e11e6ad9e7
cscnudi <106224155+cscnudi@users.noreply.github.com>
2022-08-08 16:06:15 +0900

    Update alg_kcoretruss.py

--- css/alg_kcoretruss.py
+++ css/alg_kcoretruss.py
@@ -1,60 +1,59 @@
 import networkx as nx
 
-def ifConditionSatisifed(H0, s, _k, _alpha) :
-    for (u,v) in H0.edges() :
-        e0 = (u, v) if int(u) < int(v) else (v, u)
-        if min(len(H0[u]), len(H0[v]) ) <= _k/_alpha and s[e0]<=_k-2:
-            return (u,v)
-    return None
+
+def ifConditionSatisifed(H0, _k, _alpha, _W) :
+    check = []
+    if _W is None:
+        for (u,v) in H0.edges():
+            if min(H0.degree(u),H0.degree(v)) <= _k/ _alpha and H0[u][v]['sup']<=_k-2:
+                check.append((u,v))
+                H0[u][v]['remove'] = 1
 
 
-def kcoreturss(G,alpha):
-    k = 2 # line 1
+    else:
+        for w in _W:
+            for v in H0[w]:
+                if min(H0.degree(w), H0.degree(v)) <= _k / _alpha and H0[w][v]['sup'] <= _k - 2:
+                    if H0[w][v]['remove'] == 0:
+                        check.append((v,w))
+                        H0[w][v]['remove'] = 1
+    if len(check) == 0:
+        return None
+    else:
+        return check
+
+
+
+def kcoretruss(G,alpha,k0):
+    # k = 2 # line 1
     H = G.copy() # line 1
-    sup_H_e = dict()
-
+    k0 = k0-1
     for u,v in H.edges(): # line 3
-        e = (u, v) if int(u) < int(v) else (v, u)
-        sup_H_e[e] = len((set(H[u]) & set(H[v]))) # line 6
+        H[u][v]['sup'] = len((set(H[u]) & set(H[v])))
+        H[u][v]['remove'] = 0
 
-    ct = dict()
+    intersection = None
     while True:
-        while True :
-            x = ifConditionSatisifed(H, sup_H_e, k, alpha)
-            if x != None:
-                (u,v) = x
-                H.remove_edge(u, v)
-                ct[(u, v)] = k
+        remove_edges = ifConditionSatisifed(H,k0,alpha, intersection)
+        if remove_edges is not None:
+            intersection = set()
+            for (u,v) in remove_edges:
+                H.remove_edge(u,v)
                 W = (set(H[u]) & set(H[v]))
+                intersection.update(set(H[u]) | set(H[v]))
                 for w in W:
-                    sup_H_e[(u, w) if int(u) < int(w) else (w, u)] = sup_H_e[(u, w) if int(u) < int(w) else (w, u)] - 1
-                    sup_H_e[(v, w) if int(v) < int(w) else (w, v)] = sup_H_e[(v, w) if int(v) < int(w) else (w, v)] - 1
-            else :
-                break
-        if len(H.edges()) != 0:
-            k = k + 1
+                    H[u][w]['sup'] = H[u][w]['sup'] - 1
+                    H[v][w]['sup'] = H[v][w]['sup'] - 1
         else:
             break
 
-    return ct
+    remove = [node for node, degree in H.degree() if degree == 0]
+    H.remove_nodes_from(remove)
 
+    return H
 
 
 def run(G, k, alpha):
     G0 = G.copy()
-    T = kcoreturss(G0, alpha)
-    ret = []
-    for (key, value) in T.items() :
-        if value >= k:
-            ret.append(key)
-    P = G.edge_subgraph(ret)
-    return nx.connected_components(P)
-
-
-
-
-if __name__ == '__main__':
-    G = nx.read_edgelist('kcoretruss.txt')
-    # G0 = run(G, k=2, alpha=1)
-    ct = kcoreturss(G, alpha=1)
-    print(ct)
+    T = kcoretruss(G0, alpha, k)
+    return nx.connected_components(T)
