38d454e351e875a52f6289d6cb12054c4c9767bd
cscnudi <106224155+cscnudi@users.noreply.github.com>
2022-09-06 02:14:27 +0900

    Update alg_ktripeak.py

--- css/alg_ktripeak.py
+++ css/alg_ktripeak.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+
 def ktripeak(G,k_):
 
     H0 = G.copy()
@@ -15,60 +16,81 @@ def ktripeak(G,k_):
             if e in Ktrussedge:
                 k[e] = kmax
                 H0.remove_edge(e[0],e[1])
+
     return k
 
 
 def computeSupport(G):
+    maxSupport = 0
     supportValue = {}
     for (u, v) in G.edges :
         cnt = len(set(G[u]) & set(G[v]))
         supportValue[(u, v)] = cnt
+        if maxSupport < cnt:
+            maxSupport = cnt
 
     nx.set_edge_attributes(G, supportValue, "sup")
-
-
-def getMinSup(E):
-    if len(E) == 0 :
-        return None
-    else :
-        return (E[0][2]['sup'])
+    return maxSupport
 
 
 def getMinSupEdge(E):
-    return (E[0][0], E[0][1])
+    for support, edgeList in E.items():
+        if len(edgeList) != 0:
+            edge = E[support].pop()
+            sup = support
+            break
+    e0, e1 = edge
+    return e0, e1, sup
 
 
-def minDegree(G, u, v):
-    if G.degree(u) < G.degree(v) :
-        return u
-    else :
-        return v
+def edgeSort(u,v):
+    e = (u, v) if int(u) < int(v) else (v, u)
+    return e
+
+
+def supportUpdate(G_,u_,v_,w_,supDict):
+    uw_sup = G_[u_][w_]['sup']
+    uw = edgeSort(u_, w_)
+    supDict[uw_sup + 1].remove(uw)
+
+    supDict[uw_sup].append(uw)
+    vw_sup = G_[v_][w_]['sup']
+    vw = edgeSort(v_, w_)
+    supDict[vw_sup + 1].remove(vw)
+    supDict[vw_sup].append(vw)
+
 
 def maxTruss(_H0):
 
     G = _H0.copy()
     T = {}
-    computeSupport(G)
-    E = sorted(G.edges(data=True), key=lambda t: t[2].get('sup', 1))
+    maxsup = computeSupport(G)
+    E = {sup: list() for sup in range(maxsup+1)}
+
+    for u, v in G.edges():
+        e0 = edgeSort(u,v)
+        E[G.edges[u,v]['sup']].append(e0)
+
+
     k_max = 2
+
     while len(G.edges) != 0 :
-        (u,v) = getMinSupEdge(E)
-
-        k = G[u][v]['sup'] + 2
-
+        u, v, k = getMinSupEdge(E)
+        k = k + 2
         k_max = max(k_max, k)
         T[(u,v)] = k_max
 
         u, v = (u, v) if G.degree(u) < G.degree(v) else (v, u)
 
-        E.pop(0)
         G.remove_edge(u, v)
 
         for w in G[u] :
             if w in G.neighbors(v) :
+
                 G[u][w]['sup'] = G[u][w]['sup'] - 1
                 G[v][w]['sup'] = G[v][w]['sup'] - 1
-                E = sorted(G.edges(data=True), key=lambda t: t[2].get('sup', 1))
+                supportUpdate(G, u, v, w, E)
+                
     return T, k_max
 
 
